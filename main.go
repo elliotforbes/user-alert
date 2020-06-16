acd4d8fcb049ca3328a0a6791709d63dd4a96849
Elliot Forbes <emforce@live.com>
2020-06-16 18:45:59 +0100

    Updates

--- main.go
+++ main.go
@@ -2,9 +2,31 @@ package main
 
 import (
 	"fmt"
-	"github.com/stianeikeland/go-rpio/v4"
+
+	"github.com/gofiber/fiber"
+	rpio "github.com/stianeikeland/go-rpio/v4"
 )
 
+func SetupBoard() {
+	err := rpio.Open()
+	if err != nil {
+		fmt.Println(err)
+	}
+	pin := rpio.Pin(10)
+	pin.Output() // Output mode
+	pin.High()
+}
+
 func main() {
-	fmt.Println("User Alert API")
+	fmt.Println("User Alert System")
+
+	SetupBoard()
+
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) {
+		c.Send("Hello, World!")
+	})
+
+	app.Listen(3000)
 }
